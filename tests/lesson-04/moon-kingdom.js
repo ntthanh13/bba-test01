9f3e5d0d288edd0888688c41e0f4afd20c5a7c26
test 01 - Tien Thanh <tienthanh13579@gmail.com>
2026-09-24 20:29:21 +0700

    fix: them ham theo de bai

--- tests/lesson-04/moon-kingdom.js
+++ tests/lesson-04/moon-kingdom.js
@@ -7,20 +7,22 @@ const characters = [
     { name: "Bowser", level: 6, health: 900 },
     { name: "Yoshi", level: 2, health: 300 },
 ]
-
-// Sử dụng hàm map để tạo ra mảng mới: charactersPowerUp
-const charactersPowerUp = characters.map(value => ({
-    name: value.name.toUpperCase(),
-    level: value.level * 2,
-    health: value.health * 3
-}));
-// Kiếm tra kết quả
-// console.log(charactersPowerUp);
-
-// Sử dụng hàm filter để lọc ra các phần tử có chỉ số health > 1000. Đặt tên mảng mới lọc được này là "possibleWinners"
-const possibleWinners = charactersPowerUp.filter(value => value.health > 1000);
-// Kiểm tra kết quả
-// console.log(possibleWinners);
+// Tạo hàm createCharacters:
+function createCharacters(char) {
+    // Sử dụng hàm map để tạo ra mảng mới: charactersPowerUp
+    const charactersPowerUp = char.map(value => ({
+        name: value.name.toUpperCase(),
+        level: value.level * 2,
+        health: value.health * 3
+    }));
+    // Sử dụng hàm filter để lọc ra các phần tử có chỉ số health > 1000. Đặt tên mảng mới lọc được này là "possibleWinners"
+    const possibleWinners = charactersPowerUp.filter(value => value.health > 1000);
+    // In kết quả
+    // console.log(possibleWinners);
+    return possibleWinners;
+};
+// Gọi hàm createCharacters
+console.log(createCharacters(characters));
 
 
 // Bài 2: Tạo hàm printLeaderboard:
@@ -29,24 +31,26 @@ const players = [
     { name: "Mario", score: 1500 },
     { name: "Luigi", score: 1200 },
     { name: "Toad", score: 600 },
-    { name: "Bowser", score: 1100 },
-    { name: "Yoshi", score: 1800 },
-    { name: "Boo", score: 300 },
+    { name: "Bowser", score: 700 },
+    { name: "Yoshi", score: 1100 },
     { name: "Peach", score: 1400 },
     { name: "Daisy", score: 800 }
 ]
 
-// Sắp xếp mảng người chơi theo thứ tự score từ cao đến thấp.
-players.sort((a, b) => b.score - a.score);
+// Tạo hàm printLeaderboard
+function printLeaderboard(playerList) {
+    // Sắp xếp mảng người chơi theo thứ tự score từ cao đến thấp.
+    playerList.sort((a, b) => b.score - a.score);
 
-// In ra bảng xếp hạng. Lưu ý: với 3 vị trí 1, 2, 3, hãy thêm huy chương phía trước
-const medal = ["Gold", "Silver", "Bronze"];
-for (i = 0; i < players.length; i++) {
-    if (i < 3) {
-        console.log(`${medal[i]} ${i + 1}. ${players[i].name} - ${players[i].score} point`);
-    } else {
-        console.log(`${i + 1}. ${players[i].name} - ${players[i].score} point`);
-    }
-}
-
-
+    // In ra bảng xếp hạng. Lưu ý: với 3 vị trí 1, 2, 3, hãy thêm huy chương phía trước
+    const medal = ["Gold", "Silver", "Bronze"];
+    for (i = 0; i < playerList.length; i++) {
+        if (i < 3) {
+            console.log(`${medal[i]} ${i + 1}. ${playerList[i].name} - ${playerList[i].score} point`);
+        } else {
+            console.log(`${i + 1}. ${playerList[i].name} - ${playerList[i].score} point`);
+        }
+    };
+};
+// Gọi hàm printLeaderboard
+printLeaderboard(players);
